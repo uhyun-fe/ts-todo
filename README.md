c943aa5c43a311e3132e97152a808af22cc8a711
uhyun-fe <feyouhyun0957@gmail.com>
2021-08-27 17:22:52 +0900

    create : ClockBox.tsx
    - styled-components 설치
    - ClockBox component 생성
    - update README.md

--- README.md
+++ README.md
@@ -15,4 +15,10 @@ npm start
 
 ```
 npx create-react-app ts-todo --template typescript
+npm install styled-components @types/styled-components styled-normalize
 ```
+
+## 참고
+
+-  [디자인 참고 사이트 1](https://medium.muz.li/todo-list-inspiration-a1d736c2718a#.p9kc7wuc8)
+-  [디자인 참고 사이트 2](https://dribbble.com/shots/6635602-Todo-List-App?utm_source=Pinterest_Shot&utm_campaign=mhdesigner&utm_content=Todo+List+App&utm_medium=Social_Share)
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+// Components
+import ClockBox from "./components/ClockBox";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <Container>
+         <ClockBox />
+      </Container>
+   );
 }
 
 export default App;
+
+const Container = styled.div`
+   margin: 0 auto;
+   padding: 20px 0;
+   width: 80%;
+   max-width: 500px;
+   height: 100vh;
+   /* background: rgba(0, 0, 0, 0.1); */
+`;
